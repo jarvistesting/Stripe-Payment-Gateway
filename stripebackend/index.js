4b76eaa54bb87813cbd8da7056b1bfb9648d5f95
Ravi Prakash <raivbca199600@gmail.com>
2020-02-25 12:44:53 +0530

    added

---
+++ stripebackend/index.js
@@ -0,0 +1,51 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")("sk_test_GU5BMXmEFZM1lY1kxGn90nAJ00drinRDOf");
+const uuid = require("uuid/v4");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("It works");
+});
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+  const idempotencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id
+    })
+    .then(customer => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country
+            }
+          }
+        },
+        { idempotencyKey }
+      );
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => console.log(err));
+});
+
+//listen
+app.listen(8282, () => console.log("Listening on port 8282"));
